1ccdde4bf3cddd15ca6799a8f8e94a2da6aa6047
Relintai <relintai@protonmail.com>
2023-02-13 19:24:39 +0100

    Removed new things from MLPPHypothesisTestingOld.

--- mlpp/hypothesis_testing/hypothesis_testing_old.h
+++ mlpp/hypothesis_testing/hypothesis_testing_old.h
@@ -9,15 +9,12 @@
 //
 
 #include "core/math/math_defs.h"
-
-#include "core/object/reference.h"
+#include "core/int_types.h"
 
 #include <tuple>
 #include <vector>
 
-class MLPPHypothesisTestingOld : public Reference {
-	GDCLASS(MLPPHypothesisTestingOld, Reference);
-
+class MLPPHypothesisTestingOld {
 public:
 	std::tuple<bool, real_t> chiSquareTest(std::vector<real_t> observed, std::vector<real_t> expected);
 
