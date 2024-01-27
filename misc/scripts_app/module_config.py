260c8691da8ef5b7cd930e6b43a44b1e70489c44
Relintai <relintai@protonmail.com>
2024-01-27 10:20:11 +0100

    Added helper scripts from pandemonium for the standalone build.

---
+++ misc/scripts_app/module_config.py
@@ -0,0 +1,7 @@
+
+engine_branch = 'master'
+
+engine_repository = [ ['https://github.com/Relintai/pmlpp.git', 'git@github.com:Relintai/pmlpp.git'], 'pmlpp', '' ]
+
+# Relative to pmlpp's directory!
+main_scsub_path = "../SCsub"
